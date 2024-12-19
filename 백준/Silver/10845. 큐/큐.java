2708b0a4a21b565ceb5db27a93940400d331ca65
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2024-12-20 00:25:28 +0900

    [Silver IV] Title: 큐, Time: 156 ms, Memory: 19336 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,101 +1,76 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-	static int[] queue = new int[10001]; // 큐 배열
-	static int first = 0;
-	static int last = 0;
-	
-   public static void main(String[] args) throws IOException{
-	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	   int N = Integer.parseInt(br.readLine());
-	   StringBuilder sb = new StringBuilder();
-	   
-	   for(int i=0; i<N; i++) {
-		   StringTokenizer st = new StringTokenizer(br.readLine());
-		   String command = st.nextToken();
-		   
-		   switch(command) {
-		   case "push":
-			   push(Integer.parseInt(st.nextToken()));
-			   break;
-		   case "pop":
-			   sb.append(pop()).append("\n");
-			   break;
-		   case "size":
-			   sb.append(size()).append("\n");
-			   break;
-		   case "empty":
-			   sb.append(empty()).append("\n");
-			   break;  
-		   case "front":
-			   sb.append(front()).append("\n");
-			   break;  
-		   case "back":
-			   sb.append(back()).append("\n");
-			   break;    
-		   }
-	   }
-	   System.out.println(sb);
-	   
-   }
+    static int[] queue = new int[10001]; // 큐 배열
+    static int first = 0;
+    static int last = 0;
 
-private static int back() {
-	// TODO Auto-generated method stub
-	if (first == last) {
-		return -1;
-	}
-	else {
-		int res = queue[last-1];
-		return res;
-	}
-}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-private static int front() {
-	// TODO Auto-generated method stub
-	if (first == last) {
-		return -1;
-	}
-	else {
-		int res = queue[first];
-		return res;
-	}
-}
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
 
-private static int empty() {
-	// TODO Auto-generated method stub
-	if (first == last) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
-}
+            switch (command) {
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    sb.append(pop()).append("\n");
+                    break;
+                case "size":
+                    sb.append(size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(empty()).append("\n");
+                    break;
+                case "front":
+                    sb.append(front()).append("\n");
+                    break;
+                case "back":
+                    sb.append(back()).append("\n");
+                    break;
+            }
+        }
+        System.out.print(sb);
+    }
 
-private static int size() {
-	// TODO Auto-generated method stub
-	return last-first;
-}
+    private static int back() {
+        if (first == last) {
+            return -1;
+        }
+        return queue[last - 1];
+    }
 
-private static int pop() {
-	// TODO Auto-generated method stub
-	if (last == first) {
-		return -1;
-	}
-	else {
-		int res = queue[first];
-		first++;
-		return res;
-	}
-}
+    private static int front() {
+        if (first == last) {
+            return -1;
+        }
+        return queue[first];
+    }
 
-private static void push(int num) {
-	// TODO Auto-generated method stub
-	queue[last] = num;
-	last++;
-}
+    private static int empty() {
+        return (first == last) ? 1 : 0;
+    }
 
+    private static int size() {
+        return last - first;
+    }
+
+    private static int pop() {
+        if (first == last) {
+            return -1;
+        }
+        return queue[first++];
+    }
+
+    private static void push(int num) {
+        queue[last++] = num;
+    }
 }
