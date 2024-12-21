b3d5bcb93c5eb45cb5571f39d1ecf1899f30a23b
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2024-12-21 22:43:47 +0900

    [Silver IV] Title: 카드2, Time: 448 ms, Memory: 57044 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -11,17 +11,18 @@ for i in range(1,N+1,1):
 
 state = 0 # 0=버리기 1=쌓기
 while True:
-    if len(queue)-first==1:
-        res = queue[first]
+    if len(queue)-first == 1:
+        res = queue[first] # 마지막 남아있는거 쳐 저장하고
         break
-    elif state==0:
+    elif state==0: # 버리기
         first+=1
         state=1
-    elif state==1:
+    elif state==1: #쌓기
         queue.append(queue[first])
         first+=1
         last+=1
         state=0
+
 
 print(res)
 
