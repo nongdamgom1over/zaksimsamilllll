a426518af74dc7a775faed7f2bf0ffc3d382df42
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2024-12-21 23:36:49 +0900

    [Silver V] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.py
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -2,11 +2,11 @@ money = int(input())  # 거스름돈 액수 입력
 res = 0  # 사용된 동전 수
 
 while money >= 0:
-    if money % 5 == 0:  # 남은 금액이 5로 나누어떨어지는 경우
+    if money % 5 == 0:  # 남은 금액이 5로 나누어 떨어질 경우
         res += money // 5
         print(res)
         break
-    money -= 2  # 5로 나누어떨어지지 않으면 2원짜리 동전 하나 사용
+    money -= 2  # 2원짜리 동전 하나를 사용
     res += 1
 else:
     print(-1)  # 거슬러 줄 수 없는 경우
