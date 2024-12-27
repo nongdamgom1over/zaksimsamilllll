855e95954f705c000447d1302954673dc2d1e7e2
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2024-12-27 22:05:47 +0900

    [Silver IV] Title: 문자열 집합, Time: 68 ms, Memory: 52176 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,15 +1,10 @@
 import sys
 
-n, m = map(int, sys.stdin.readline().rstrip().split(" "))
-s=[]
-cnt = 0
-for _ in range(n): # S에 포함되어야 하는 문자열들
-    str = sys.stdin.readline().rstrip()
-    s.append(str)
+input = sys.stdin.read
+data = input().splitlines()
 
-for _ in range(m):
-    str = sys.stdin.readline().rstrip() # 비교할 거
-    if str in s:
-        cnt+=1
+n, m = map(int, data[0].split())
+s = set(data[1:n+1])
+cnt = sum(1 for str in data[n+1:n+1+m] if str in s)
 
 print(cnt)
