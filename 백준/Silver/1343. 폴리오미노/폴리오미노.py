e0bc0f7e3933cbf600283c653f65b4306902b3bc
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2025-01-05 03:16:21 +0900

    [Silver V] Title: 폴리오미노, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -0,0 +1,13 @@
+import sys
+
+board = sys.stdin.readline().rstrip()
+
+board = board.replace("XXXX", "AAAA")
+board = board.replace("XX", "BB")
+
+if 'X' in board:
+    print(-1)
+
+else:
+    print(board)
+
