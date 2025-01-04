cd31878980ee03cba78daf0f6f4255bbac579bc6
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2025-01-05 00:03:43 +0900

    [Silver I] Title: 배열 돌리기, Time: 3192 ms, Memory: 55980 KB -BaekjoonHub

--- 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
+++ 백준/Silver/17276. 배열 돌리기/배열 돌리기.py
@@ -1,46 +1,42 @@
-import sys  
-from copy import deepcopy  
-  
-T = int(input())  
-for tc in range(T):  
-    N, rotate = map(int, input().split())  
-    arr = [list(map(int, input().split())) for _ in range(N)]  
-    # 결과 저장할 배열  
-    result = [[0]*N for _ in range(N)]  
-  
-    if rotate < 0:  
-        rotate = 360 + rotate  
-    # 제자리 일 경우 그대로 출력  
-    if rotate == 360 or rotate == 0:  
-        for l in arr:  
-            print(*l)  
-    else:  
-        # 45의 배수로 나오므로 나눈 몫만큼 반복해주어야 한다.  
-        for _ in range(rotate//45):  
-            for i in range(N):  
-                for j in range(N):  
-                    # 주 대각선이라면 가운데 행을 들고 와준다.  
-                    if i==j:  
-                        result[i][j] = arr[N//2][j]  
-                    # 가운데 행이라면 부 대각선을 들고와준다.  
-                    elif i == N//2:  
-                        result[i][j] = arr[N-j-1][j]  
-                    # 부 대각선이라면 가운데 열을 들고온다  
-                    # 부 대각선 좌표 방법 2가지                    
-                    # i+j = N-1                    
-                    # N-j-1 = i                    
-                    elif i+j == N-1:  
-                        result[i][j] = arr[i][N//2]  
-                    # 가운데 열이라면 주 대각선을 들고온다.  
-                    elif j == N//2:  
-                        result[i][j] = arr[i][i]  
-  
-                    # 모두 아니라면 같은 좌표값 들고오기  
-                    else:  
-                        result[i][j] = arr[i][j]  
-            # 회전을 더해야할수도 있으므로 복사해주기  
-            arr = deepcopy(result)  
-  
-        for k in result:  
-            print(*k)  
-        # print('=' * 20)
+import sys
+from copy import deepcopy
+input = sys.stdin.readline
+
+T = int(input().strip())
+for _ in range(T):
+    N, rotate = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    # 음수 회전을 양수로 변환
+    if rotate < 0:
+        rotate = 360 + rotate
+
+    # 45의 배수만큼 반복
+    r = rotate // 45
+
+    # 제자리일 경우 그대로 출력
+    if rotate == 0 or rotate == 360:
+        for row in arr:
+            print(*row)
+        continue
+
+    # 반복적으로 회전 처리
+    for _ in range(r):
+        result = [[0] * N for _ in range(N)]
+        for i in range(N):
+            for j in range(N):
+                if i == j:  # 주 대각선 -> 중앙 열
+                    result[i][j] = arr[N // 2][j]
+                elif i == N // 2:  # 중앙 행 -> 부 대각선
+                    result[i][j] = arr[N - j - 1][j]
+                elif i + j == N - 1:  # 부 대각선 -> 중앙 열
+                    result[i][j] = arr[i][N // 2]
+                elif j == N // 2:  # 중앙 열 -> 주 대각선
+                    result[i][j] = arr[i][i]
+                else:  # 나머지 유지
+                    result[i][j] = arr[i][j]
+        arr = deepcopy(result)
+
+    # 결과 출력
+    for row in arr:
+        print(*row)
