7282c44a472d6070a5b554a1538d770cdc286efa
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2025-03-19 21:56:38 +0900

    [level 0] Title: 두 수의 곱 구하기, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/120804. 두 수의 곱 구하기/두 수의 곱 구하기.py
@@ -0,0 +1,10 @@
+def solution(num1, num2):
+    answer = 0
+    answer = num1*num2
+    return answer
+
+if __name__ == '__main__':
+    num1 = int(input())
+    num2 = int(input())
+    
+    result = solution(num1, num2)
