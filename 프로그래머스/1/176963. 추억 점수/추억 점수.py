51d1fe6d99e526ce4f44377aef484706ed8f3f82
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2025-11-27 17:18:57 +0900

    [level 1] Title: 추억 점수, Time: 1.79 ms, Memory: 9.77 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -3,16 +3,14 @@ def solution(name, yearning, photo):
     n = len(name)
 
     # 1. name과 yearning value 매핑
-    score = {}
-    for i in range(n):
-        score[name[i]] = yearning[i]
+    score = {name[i] : yearning[i] for i in range(n)}
     # 딕셔너리 컴프리헨션 : score = {name[i]: yearning[i] for i in range(n)}
 
     # 2: 각 photo의 사람들 이름 보고 점수 합산
     for people in photo:
         total = 0
         for person in people:
-            total += score.get(person, 0) # 없으면 0
+            total += score.get(person, 0) # 기본 값 0
         answer.append(total)
 
     return answer
