95b966ae04fee01fdef5eeb9c606545ac4699b3a
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2026-01-15 21:42:59 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,26 @@
+def solution(storey):
+    answer = 0
+    i = 0
+    while storey>0:
+        digit = storey % 10
+        next_digit = (storey // 10) % 10
+
+        if digit > 5: # 5보다 크면 층 올라가는게 빠름
+            answer += (10 - digit)
+            storey+=10
+            i+=1
+        elif digit<5 : # 내려가기
+            answer += digit
+        else: # digit == 5
+            # 다음 자리가 5이상이면 올림, 아니면 내림
+            if next_digit >= 5:
+                answer+=5
+                storey+=10
+            else:
+                answer+=5
+
+        storey //= 10
+    return answer
+
+storey = 2554
+print(solution(storey))
