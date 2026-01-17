252f61b2beafa9f1fd10d068017adaa239cb0493
현수 <89917739+nongdamgom1over@users.noreply.github.com>
2026-01-18 04:00:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.90 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,23 +1,22 @@
 from collections import deque
 def solution(maps):
-    answer = 0
-    row,col = len(maps), len(maps[0])
-    dist = [[0]*col for _ in range(row)]
-    q = deque([(0,0)])
+    m, n = len(maps), len(maps[0])
+    dist = [[0]*n for _ in range(m)]
+    q = deque([(0,0)]) # 시작점
     dist[0][0] = 1
 
     while q:
-        x,y = q.popleft()
-        if (x,y) == (row-1,col-1):
+        x, y = q.popleft()
+        if (x,y) == (m-1,n-1): # 상대 진영 도착
             return dist[x][y]
-        for dx, dy in ((0,1),(0,-1),(1,0),(-1,0)):
+        for dx,dy in ((0,1),(0,-1),(1,0),(-1,0)):
             nx, ny = x+dx, y+dy
-            if 0<=nx<row and 0<=ny<col:
+            if 0<=nx<m and 0<=ny<n:
                 if dist[nx][ny] == 0 and maps[nx][ny] == 1:
-                    dist[nx][ny] = dist[x][y] + 1
                     q.append((nx,ny))
+                    dist[nx][ny] = dist[x][y] + 1
 
     return -1
 
-maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
 print(solution(maps))
